9fae49c5e885d3dfa01a68e3fff57318e1440ebe
marcojb05 <antonio2552001@outlook.com>
2023-09-13 11:36:10 -0600

    Se agregaron las tablas a la base de datos.

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-# Creación de una tabla con dependencias
+""" # Creación de una tabla con dependencias
 class Genero(models.Model):
     idGenero = models.IntegerField(primary_key=True, db_column='idGenero')
     tipoGenero = models.TextField(db_column='tipoGenero')
@@ -24,4 +24,95 @@ class alumno_has_genero (models.Model):
     fk_alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, db_column='fk_alumno')
     fk_genero = models.ForeignKey(Genero, on_delete=models.CASCADE, db_column='fk_genero')
     class Meta:
-        db_table = 'alumno_has_genero'
+        db_table = 'alumno_has_genero' """
+        
+# Tabla CategoriasGastos
+class CategoriasGastos (models.Model):
+    idCategoriaGasto = models.AutoField(primary_key=True, default=1, db_column='idCategoriaGasto')
+    categoria= models.CharField(max_length=100, db_column='categoria')
+    class Meta:
+        db_table="CategoriasGastos"
+    
+# Tabla MetodosPago
+class MetodosPago (models.Model):
+    idMetodo = models.AutoField(primary_key=True, default=1, db_column='idMetodo')
+    metodo= models.CharField(max_length=30, db_column='metodo')
+    class Meta:
+        db_table="MetodosPago"
+        
+# Tabla TiposMoneda
+class TiposMoneada (models.Model):
+    idTipoMoneda = models.AutoField(primary_key=True, default=1, db_column='idTipoMoneda')
+    tipoMoneda= models.CharField(max_length=30, db_column='tipoMoneda')
+    class Meta:
+        db_table="TiposMoneda"
+        
+# Tabla MetasFinancieras
+class MetasFinancieras (models.Model):
+    idMeta = models.AutoField(primary_key=True, default=1, db_column='idMeta')
+    descripcion= models.CharField(max_length=100, db_column='descripcion')
+    cantidad = models.IntegerField(db_column='cantidad')
+    fecha = models.DateField(db_column='fecha')
+    class Meta:
+        db_table="MetasFinancieras"
+        
+# Tabla CuentaBancaria
+class CuentaBancaria (models.Model):
+    idCuenta = models.AutoField(primary_key=True, db_column='idMetodo')
+    numCuenta = models.CharField(max_length=18, db_column='numCuenta')
+    fk_idMetodo = models.ForeignKey(MetodosPago, on_delete=models.CASCADE, db_column='fk_idMetodo')
+    class Meta:
+        db_table="CuentaBancaria"
+    
+# Tabla Ingresos
+class Ingresos (models.Model):
+    idIngreso = models.IntegerField(primary_key=True, db_column='idIngreso')
+    cantidad = models.IntegerField(db_column='cantidad')
+    descripcion = models.CharField(max_length=100, db_column='descripcion')
+    fecha = models.DateField(db_column='fecha')
+    fk_CategoriaIngreso = models.ForeignKey(CategoriasGastos, on_delete=models.CASCADE, db_column='fk_idCategoriaIngreso')
+    fk_idTipoMoneda = models.ForeignKey(TiposMoneada, on_delete=models.CASCADE, db_column='fk_idTipoMoneda')
+    class Meta:
+        db_table="Ingresos"
+        
+# Tabla Gastos
+class Gastos (models.Model):
+    idGasto = models.IntegerField(primary_key=True, db_column='idIngreso')
+    cantidad = models.IntegerField(db_column='cantidad')
+    descripcion = models.CharField(max_length=100, db_column='descripcion')
+    fecha = models.DateField(db_column='fecha')
+    fk_CategoriaIngreso = models.ForeignKey(CategoriasGastos, on_delete=models.CASCADE, db_column='fk_idCategoriaIngreso')
+    fk_idTipoMoneda = models.ForeignKey(TiposMoneada, on_delete=models.CASCADE, db_column='fk_idTipoMoneda')
+    fk_idMetodoPago = models.ForeignKey(MetodosPago, on_delete=models.CASCADE, db_column='fk_idMetodoPago')
+    class Meta:
+        db_table="Gastos"
+        
+# Tabla Ahorros
+class Ahorros (models.Model):
+    idAhorro = models.IntegerField(primary_key=True, db_column='idAhorro')
+    cantidad = models.IntegerField(db_column='cantidad')
+    descripcion = models.CharField(max_length=100, db_column='descripcion')
+    fecha = models.DateField(db_column='fecha')
+    fk_CategoriaIngreso = models.ForeignKey(CategoriasGastos, on_delete=models.CASCADE, db_column='fk_idCategoriaIngreso')
+    class Meta:
+        db_table="Ahorros"
+        
+# Tabla Deudas
+class Deudas (models.Model):
+    idDeuda = models.IntegerField(primary_key=True, db_column='idDeuda')
+    cantidad = models.IntegerField(db_column='cantidad')
+    descripcion = models.CharField(max_length=100, db_column='descripcion')
+    fechaPago = models.DateField(db_column='fechaPago')
+    fk_idTipoMoneda = models.ForeignKey(TiposMoneada, on_delete=models.CASCADE, db_column='fk_idTipoMoneda')
+    class Meta:
+        db_table="Deudas"
+        
+# Tabla BalanceGeneral
+class BalanceGeneral (models.Model):
+    idBalance = models.IntegerField(primary_key=True, db_column='idDeuda')
+    fk_idGasto = models.ForeignKey(Gastos, on_delete=models.CASCADE, db_column='fk_idGasto')
+    fk_idIngreso = models.ForeignKey(Ingresos, on_delete=models.CASCADE, db_column='fk_idIngreso')
+    fk_idDeuda = models.ForeignKey(Deudas, on_delete=models.CASCADE, db_column='fk_idDeuda')
+    fk_idMeta = models.ForeignKey(MetasFinancieras, on_delete=models.CASCADE, db_column='fk_idMeta')
+    class Meta:
+        db_table="BalanceGeneral"

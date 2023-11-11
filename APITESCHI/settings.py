7ecca8a92a5815f200e71ddb65862c1eb611e636
marcojb05 <antonio2552001@oulook.com>
2023-11-11 11:18:01 -0600

    Cambio de métodos decoradores.

--- APITESCHI/settings.py
+++ APITESCHI/settings.py
@@ -148,7 +148,7 @@ if not DEBUG:
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-LOGIN_URL = 'login'
+LOGIN_URL = '/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
